ae52850cd302ce112483a21828dfb946f1e5b7cf
hojachen <hojachen@tencent.com>
2024-03-13 20:11:32 +0800

    feat: 添加husky钩子

---
+++ .commitlintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'build',
+        'revert',
+        'merge',
+      ],
+    ],
+  },
+};
